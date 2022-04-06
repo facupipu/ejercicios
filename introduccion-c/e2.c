96da08f900397033bea3d45700942338d1789147
facupipu <102254049+facupipu@users.noreply.github.com>
2022-04-06 15:30:08 -0300

    e2.c

--- introduccion-c/e2.c
+++ introduccion-c/e2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hola, %s, %s, %s", argv[3], argv[2], argv[1]);
+  printf("Hola %s, %s y %s", argv[3], argv[2], argv[1]);
   return 0;
 }
